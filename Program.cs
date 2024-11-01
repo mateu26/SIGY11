6a03e56f06a5ad04ada10cef321af9b0d295b1b5
Matthew Mateu Molepo <mateumolepo11@gmail.com>
2024-11-01 14:30:26 +0200

    Added advertisements for NVIDIA, Microsoft Azure, eToro, IG Markets, Amazon Affiliate Program, and AliExpress Affiliate Program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using SIGY11.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
